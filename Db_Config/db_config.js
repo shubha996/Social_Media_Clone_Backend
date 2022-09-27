460aed20072970a9924ed7922c046332317c8d2e
ShubhamGawai <shubham.gawai9@gmail.com>
2022-09-27 09:48:35 +0530

    Added DB to server and changed config files.

--- Db_Config/db_config.js
+++ Db_Config/db_config.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    database: 'friends_book'
+    user: 'id19630153_shubham_gawai',
+    database: 'id19630153_friends_book',
+    password: ']^Ts?k?j$nZp|Pw9'
 });
 
 module.exports = connection;
